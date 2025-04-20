a0bba927cbe51fda74ee8c469d67ac2bbbd7a962
ismetmercanli <mercanliismet@gmail.com>
2025-04-20 23:35:36 +0300

    Dil Desteği dosyaları eklendi

---
+++ Dil Desteği/i18n.js
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import translationEN from "./locales/en/translation.json";
+import translationTR from "./locales/tr/translation.json";
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  tr: {
+    translation: translationTR,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "tr",
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
